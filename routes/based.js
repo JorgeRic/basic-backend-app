f0d1859a15e1f516ce0ba3ac5ee60ff7b138b9d2
Jorge <jorrillop@hotmail.com>
2019-08-14 15:46:15 +0200

    rutas frontend creadas

--- routes/based.js
+++ routes/based.js
@@ -28,12 +28,55 @@ router.post('/viv/new', async (req, res, next) => {
 
 //modifica las viviendas
 router.put('/viv/:id/edit', (req, res ,next) => {
+  const {id} = req.params
+  const vivupdated = req.body
+  try{
+    const updated = await Aplication.findByIdAndUpdate(id, vivupdated, {new: true})
+    res.status(200).json(updated)
+  }catch(error){
+    next(error)
+  }
 
 })
 
 //borra las viviendas
 router.delete('/viv/:id/delete', (req, res,next) => {
+  const {id}
+  try{
+    await Aplication.findByIdAndDelete(id)
+    res.status(200).json({message: 'Vivienda eliminada'})
+  }catch(error){
+    next(error)
+  }
 
 })
+
+router.get('/details/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const detail = await Aplication.findById(id)
+    res.status(200).json(detail)
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+// router.get('/search', async (req, res, next) => {
+//   try {
+//     const referencia = req.query.referencia;
+//     const refer = await Aplication.find({ referencia });
+
+//     res.status(200).json(refer)
+
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+
+
+
+
 
 module.exports = router
